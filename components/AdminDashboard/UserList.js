ee19a5e1ff6c7e7ffcb5852cbee3d7b866b422c4
Derek Garnett <drkgrntt@gmail.com>
2019-08-18 16:39:03 -0500

    Display user info in userlist

--- components/AdminDashboard/UserList.js
+++ components/AdminDashboard/UserList.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { setUsers } from '../../reduxStore'
 import Modal from '../Modal'
 
 
-class UserList extends Component { 
+class UserList extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { selectedUser: '' }
+  }
+
+
+  renderUserInfo(user) {
+
+    const visible = user._id === this.state.selectedUser ? true : false
+
+    return (
+      <ul className={`user-list__info${visible ? ' user-list__info--visible' : ''}`}>
+        <li>Email: {user.email}</li>
+        <li>First Name: {user.firstName}</li>
+        <li>Last Name: {user.lastName}</li>
+        <li>Admin: {user.isAdmin.toString()}</li>
+      </ul>
+    )
+  }
+
 
   renderUsers() {
 
@@ -13,8 +35,17 @@ class UserList extends Component {
     return users.map(user => {
 
       return (
-        <li key={user._id}>
-          {user.email}
+        <li key={user._id} className="user-list__user">
+          <div className="user-list__item">
+            <span className="user-list__email">{user.email}</span>
+            <button
+              onClick={() => this.setState({ selectedUser: user._id })}
+              className="user-list__check-info button button-small button-secondary"
+            >
+              Info
+            </button>
+          </div>
+          {this.renderUserInfo(user)}
         </li>
       )
     })
