9e69195e69682c49dd460d0c5ca8f029394d3e9f
Derek Garnett <drkgrntt@gmail.com>
2019-08-21 18:38:28 -0500

    Include a dev-personal environment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod')
-} else {
+} else if (process.env.NODE_ENV === 'personal') {
   module.exports = require('./devPersonal')
+} else {
+  module.exports = require('./dev')
 }
