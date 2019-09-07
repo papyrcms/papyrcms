2ea4b201884bfbb53b4a418cf9e4d6f5acf32d2a
Derek Garnett <drkgrntt@gmail.com>
2019-09-07 16:28:11 -0500

    new landing page

--- components/Sections/SectionMedia.js
+++ components/Sections/SectionMedia.js
@@ -6,7 +6,7 @@ import Media from '../Media'
 /**
  * SectionMedia will render an image or video across the width
  * of the screen
- * 
+ *
  * @prop className - String - The classname to prefix each class for the media
  * @prop posts - Array[Object - One post to be rendered]
  * @prop fixed - Boolean - Determine whether the media will be fixed to the background or scroll with the view
@@ -19,22 +19,22 @@ const SectionMedia = props => {
     const { title, content, mainMedia } = props.post
     const { fixed, alt } = props
     const className = props.className ? props.className : 'section-media'
-  
+
     return (
       <section className={`${className}${fixed ? '--fixed' : ''}`}>
-  
+
         <div className={`${className}__text`}>
           <h2 className={`${className}__title`}>{title}</h2>
           <div className={`${className}__subtext`}>{renderHTML(content)}</div>
         </div>
-  
+
         <Media
           className={`${className}__media${fixed ? '--fixed' : ''}`}
           src={mainMedia}
           alt={alt}
           parallax={fixed}
         />
-  
+
       </section>
     )
   } else {
---
+++ pages/displays.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import PostsFilter from '../components/PostsFilter'
+import { 
+  SectionCards,
+  SectionMedia,
+  SectionStandard,
+  SectionSlideshow,
+  SectionMaps
+} from '../components/Sections/'
+
+const LandingPage = props => (
+  <div className="landing">
+    <PostsFilter
+      component={SectionCards}
+      posts={props.posts}
+      settings={{
+        maxPosts: 3,
+        postTags: 'sample'
+      }}
+      componentProps={{
+        title: 'This is the 3 Card Section',
+        contentLength: 200,
+        readMore: true,
+        perRow: 3,
+        emptyMessage: 'Create content with the "sample" tag.',
+        clickableMedia: true
+      }}
+    />
+    <PostsFilter
+      component={SectionMedia}
+      posts={props.posts}
+      settings={{
+        maxPosts: 1,
+        postTags: 'video-section'
+      }}
+      singular
+      componentProps={{
+        emptyTitle: 'Video Section',
+        emptyMessage: 'Create content with the "video-section" tag'
+      }}
+    />
+    <PostsFilter
+      component={SectionStandard}
+      posts={props.posts}
+      settings={{
+        postTags: 'sample',
+        maxPosts: 2,
+      }}
+      componentProps={{
+        title: 'This is the Standard Section',
+        readMore: true,
+        emptyMessage: 'Create content with the "sample" tag.',
+        clickableMedia: true
+      }}
+    />
+    <PostsFilter
+      component={SectionMedia}
+      posts={props.posts}
+      singular
+      settings={{
+        postTags: 'parallax-section',
+        maxPosts: 1,
+      }}
+      componentProps={{
+        fixed: true,
+        alt: 'Parallax Image',
+        emptyTitle: 'This is the parallax section',
+        emptyMessage: 'Create content with the "parallax-section" tag.'
+      }}
+    />
+    <PostsFilter
+      component={SectionCards}
+      posts={props.posts}
+      settings={{
+        maxPosts: 4,
+        postTags: 'sample'
+      }}
+      componentProps={{
+        title: '4 Card Section',
+        contentLength: 100,
+        perRow: 4,
+        readMore: true,
+        emptyMessage: 'Create content with the "sample" tag.',
+        clickableMedia: true
+      }}
+    />
+    <PostsFilter
+      component={SectionSlideshow}
+      posts={props.posts}
+      settings={{
+        maxPosts: 4,
+        postTags: 'slideshow-section'
+      }}
+      componentProps={{
+        timer: 5000,
+        emptyTitle: 'This is the slideshow section',
+        emptyMessage: 'Create content with the "slideshow-section" tag.'
+      }}
+    />
+    <PostsFilter
+      component={SectionMaps}
+      posts={props.posts}
+      settings={{
+        maxPosts: 3,
+        postTags: ['maps-section', 'main'],
+        strictTags: true
+      }}
+      componentProps={{
+        mapLocation: 'end',
+        emptyTitle: 'This is the maps section',
+        emptyMessage: 'Create content with the "maps-section" and "main" tags. Be sure to include a post with the "latitude" tag and "logintude" tag as well.'
+      }}
+    />
+  </div>
+)
+
+
+LandingPage.getInitialProps = async context => {
+
+  let googleMapsKey = ''
+
+  if (!!context.res) {
+    googleMapsKey = context.query.googleMapsKey
+  } else {
+    const res = await axios.post('/api/googleMapsKey')
+    googleMapsKey = res.data
+  }
+
+  return { googleMapsKey }
+}
+
+
+const mapStateToProps = state => {
+  return { posts: state.posts }
+}
+
+
+export default connect(mapStateToProps)(LandingPage)
