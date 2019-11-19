837a336045cee7aaab8497f616467e0cda4bf003
Derek Garnett <drkgrntt@gmail.com>
2019-11-18 23:11:31 -0600

    swap landing pages

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,128 @@
 import React from 'react'
+import axios from 'axios'
 import filterPosts from '../components/filterPosts'
 import {
   SectionCards,
   SectionMedia,
   SectionStandard,
-} from '../components/Sections/'
-
+  SectionSlideshow,
+  SectionMaps
+} from '../components/Sections'
 
 const LandingPage = props => (
-
   <div className="landing">
 
+    <SectionCards
+      posts={props.threeCardPosts}
+      title="This is the 3 Card Section"
+      contentLength={200}
+      readMore
+      perRow={3}
+      emptyMessage="Create content with the 'sample' tag."
+      clickableMedia
+    />
+
+    <SectionMedia
+      post={props.videoPosts[0]}
+      emptyTitle="Video Section"
+      emptyMessage="Create content with the 'video-section' tag."
+    />
+
     <SectionStandard
-      posts={props.goalsPosts}
-      title="Our Mission"
-      contentLength={300}
-      readMore={false}
+      posts={props.standardPosts}
+      title="This is the Standard Section"
+      emptyMessage="Create content with the 'sample' tag."
+      readMore
+      clickableMedia
     />
 
     <SectionMedia
       post={props.parallaxPosts[0]}
-      fixed
-      alt="code"
+      alt="Parallax Image"
       emptyTitle="This is the parallax section"
-      emptyMessage="Create Content with the 'landing-parallax' tag."
+      emptyMessage="Create content with the 'parallax-section' tag."
+      fixed
     />
 
     <SectionCards
-      posts={props.portfolioPosts}
-      title="Our work"
-      contentLength={120}
+      posts={props.fourCardPosts}
+      title="4 Card Section"
+      contentLength={100}
+      perRow={4}
+      emptyMessage="Create content with the 'sample' tag."
       readMore
-      perRow={3}
+      clickableMedia
     />
 
+    <SectionSlideshow
+      posts={props.slideshowPosts}
+      timer={5000}
+      emptyTitle="This is the slideshow section"
+      emptyMessage="Create content with the 'slideshow-section' tag."
+    />
+
+    <SectionMaps
+      posts={props.mapsPosts}
+      mapLocation="end"
+      emptyTitle="This is the maps section"
+      emptyMessage="Create content with the 'maps-section' and 'main' tags. Be sure to include a post with the 'latitude' tag and 'longitude' tag as well."
+    />
   </div>
 )
 
 
+LandingPage.getInitialProps = async context => {
+
+  let googleMapsKey = ''
+
+  if (!!context.res) {
+    googleMapsKey = context.query.googleMapsKey
+  } else {
+    const res = await axios.post('/api/googleMapsKey')
+    googleMapsKey = res.data
+  }
+
+  return { googleMapsKey }
+}
+
+
 const settings = [
   {
-    propName: 'goalsPosts',
-    postTags: 'landing-goals',
+    propName: 'threeCardPosts',
     maxPosts: 3,
+    postTags: 'sample'
+  },
+  {
+    propName: 'videoPosts',
+    maxPosts: 1,
+    postTags: 'video-section'
+  },
+  {
+    propName: 'standardPosts',
+    postTags: 'sample',
+    maxPosts: 2,
   },
   {
     propName: 'parallaxPosts',
-    postTags: 'landing-parallax',
-    maxPosts: 1
+    postTags: 'parallax-section',
+    maxPosts: 1,
   },
   {
-    propName: 'portfolioPosts',
+    propName: 'fourCardPosts',
+    maxPosts: 4,
+    postTags: 'sample'
+  },
+  {
+    propName: 'slideshowPosts',
+    maxPosts: 4,
+    postTags: 'slideshow-section'
+  },
+  {
+    propName: 'mapsPosts',
     maxPosts: 3,
-    postTags: ['portfolio', 'web'],
+    postTags: ['maps-section', 'main'],
     strictTags: true
-  }
+  },
 ]
 
 
