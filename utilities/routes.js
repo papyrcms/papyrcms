df462910d3294d82f91976a6fbd168fbe9345447
Derek Garnett <drkgrntt@gmail.com>
2019-12-01 21:41:55 -0600

    make endpoint for certificate

--- utilities/routes.js
+++ utilities/routes.js
@@ -16,6 +16,12 @@ class UtilityRoutes {
     this.server.get('/', this.renderLanding.bind(this))
     this.server.post('/api/googleAnalyticsId', this.sendGoogleAnalyticsId)
     this.server.post('/api/googleMapsKey', this.sendGoogleMapsKey)
+    this.server.get('/.well-known/acme-challenge/jF3NwtV-cPO87squ85GYih5h-8bo1rnMqz5L9UcZxSw', this.certbot)
+  }
+
+
+  certbot(req, res) {
+    res.send('jF3NwtV-cPO87squ85GYih5h-8bo1rnMqz5L9UcZxSw.L34vb6OgvUvit8D5FoBZoKO0Jjf-n3dlqtKE0cLzs8I')
   }
 
 
