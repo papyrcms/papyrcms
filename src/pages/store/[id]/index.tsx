25542d616253a092da3f6709cad4287e8601f1a6
drkgrntt <drkgrntt@gmail.com>
2020-03-25 23:34:33 -0500

    add purchase options to product page

--- src/pages/store/[id]/index.tsx
+++ src/pages/store/[id]/index.tsx
@@ -1,8 +1,11 @@
 import React, { Fragment, useState, useEffect, useContext } from 'react'
 import { NextPageContext } from 'next'
+import Link from 'next/link'
 import axios from 'axios'
+import _ from 'lodash'
 import { useRouter } from 'next/router'
 import userContext from '../../../context/userContext'
+import storeContext from '../../../context/storeContext'
 import keys from '../../../config/keys'
 import { SectionStandard } from '../../../components/Sections'
 
@@ -13,6 +16,7 @@ type Props = {
 const StoreShow = (props: Props) => {
 
   const { currentUser } = useContext(userContext)
+  const { cart, addToCart } = useContext(storeContext)
   const [product, setProduct] = useState(props.product)
   const { query } = useRouter()
 
@@ -32,8 +36,39 @@ const StoreShow = (props: Props) => {
       <Fragment>
         <p>${product.price.toFixed(2)}</p>
         <p>{product.quantity} in stock</p>
+        {renderCheckout(product)}
       </Fragment>
     )
+  }
+
+  const renderAddToCart = (product: Product) => {
+    const quantityInCart = _.filter(cart, cartProduct => cartProduct._id === product._id).length
+    let message = 'Add to cart'
+    if (quantityInCart) message += ` (${quantityInCart} now)`
+
+    return (
+      <a onClick={async event => {
+        event.preventDefault()
+        await addToCart(product)
+      }} href="#">
+        {message}
+      </a>
+    )
+  }
+
+
+  const renderCheckout = (product: Product) => {
+    if (product.quantity > 0) {
+      return (
+        <Fragment>
+          {renderAddToCart(product)}
+          <br />
+          <Link href={`/store/checkout?id=${product._id}`}>
+            <a>Buy it now</a>
+          </Link>
+        </Fragment>
+      )
+    }
   }
 
   return (
