830c7dfd81ff38f003c396a1f035e4f341a66720
drkgrntt <drkgrntt@gmail.com>
2020-06-28 08:02:14 -0500

    Make button component

---
+++ src/components/Button.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+
+const Button = (props) => {
+
+  const {
+    // Standard button props
+    className = '',
+    disabled = false,
+    onClick = () => {},
+    id = '',
+    style = {},
+    title = '',
+    children = '',
+
+    // Custom button props
+    type = 'primary', // I know type is a standard prop, but it's a stupid standard prop
+    submittedText = children
+  } = props
+
+  const [buttonDisabled, setButtonDisabled] = useState(disabled)
+  const [buttonText, setButtonText] = useState(children)
+
+  const actualClassName = `button button-${type} ${className}`
+  
+  const handleClick = (event) => {
+    setButtonText(submittedText)
+    setButtonDisabled(true)
+
+    const reset = () => {
+      setButtonText(children)
+      setButtonDisabled(false)
+    }
+
+    onClick(event, reset)
+  }
+
+  return (
+    <button
+      className={actualClassName}
+      disabled={buttonDisabled}
+      onClick={handleClick}
+      id={id}
+      title={title}
+      style={style}
+    >
+      {buttonText}
+    </button>
+  )
+}
+
+
+export default Button
