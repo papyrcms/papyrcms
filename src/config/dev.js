c3dbc9f7e454f447834330783c88f438d8dc901d
drkgrntt <drkgrntt@gmail.com>
2020-06-28 18:11:45 -0500

    Add empty dev.js to git history

---
+++ src/config/dev.js
@@ -0,0 +1,29 @@
+export default {
+  stripePublishableKey: '',
+  stripeSecretKey: '',
+
+  gmailClientId: '',
+  gmailClientSecret: '',
+  gmailRefreshToken: '',
+
+  googleAnalyticsId: '',
+  googleMapsKey: '',
+
+  siteEmail: '',
+  adminEmail: '',
+
+  cloudinaryApiKey: '',
+  cloudinaryApiSecret: '',
+  cloudinaryCloudName: '',
+
+  mongoURI: '',
+  rootURL: '',
+  jwtSecret: '',
+
+  test: {
+    oldPass: '',
+    newPass: '',
+    token: "",
+    tokenRpc: ''
+  }
+}
