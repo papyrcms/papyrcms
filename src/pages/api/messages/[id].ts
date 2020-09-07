3078d3b942c5f80aa3800486757cf9457bde862e
drkgrntt <drkgrntt@gmail.com>
2020-09-06 19:29:26 -0500

    convert messages api pages

--- src/pages/api/messages/[id].ts
+++ src/pages/api/messages/[id].ts
@@ -1,18 +1,17 @@
-import serverContext from "@/serverContext"
+import { NextApiRequest, NextApiResponse } from 'next'
+import serverContext from '@/serverContext'
 
-
-export default async (req, res) => {
-
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { user, done, database } = await serverContext(req, res)
 
   if (!user || !user.isAdmin) {
-    return done(403, { message: "You are not allowed to do that." })
+    return done(403, { message: 'You are not allowed to do that.' })
   }
 
   if (req.method === 'DELETE') {
     const { destroy, Message } = database
     await destroy(Message, { _id: req.query.id })
-    return done(200, "message deleted")
+    return done(200, 'message deleted')
   }
 
   return done(404, { message: 'Page not found.' })
--- src/pages/api/messages/index.ts
+++ src/pages/api/messages/index.ts
@@ -1,28 +1,36 @@
+import { Database } from 'types'
+import { NextApiRequest, NextApiResponse } from 'next'
 import serverContext from '@/serverContext'
-import Mailer from "@/utilities/mailer"
-import keys from "@/keys"
+import Mailer from '@/utilities/mailer'
+import keys from '@/keys'
 
-
-const getMessages = async (database) => {
+const getMessages = async (database: Database) => {
   const { findAll, Message } = database
   return await findAll(Message, {}, { sort: { created: -1 } })
 }
 
-
-const createMessage = async (body, enableEmailingToAdmin, database) => {
-
+const createMessage = async (
+  body: any,
+  enableEmailingToAdmin: boolean,
+  database: Database
+) => {
   const messageBody = {
     name: body.name,
     email: body.email,
     message: body.message,
-    emailSent: false
+    emailSent: false,
   }
 
   if (enableEmailingToAdmin) {
     const mailer = new Mailer(database)
     const subject = `New message from ${messageBody.name}!`
 
-    const sent = mailer.sendEmail(messageBody, keys.adminEmail, "contact", subject)
+    const sent = mailer.sendEmail(
+      messageBody,
+      keys.adminEmail,
+      'contact',
+      subject
+    )
 
     if (sent) {
       messageBody.emailSent = true
@@ -33,22 +41,28 @@ const createMessage = async (body, enableEmailingToAdmin, database) => {
   return await create(Message, messageBody)
 }
 
-
-export default async (req, res) => {
-
-  const { user, settings, done, database } = await serverContext(req, res)
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { user, settings, done, database } = await serverContext(
+    req,
+    res
+  )
 
   if (req.method === 'GET') {
     if (!user || !user.isAdmin) {
-      return await done(403, { message: 'You are not allowed to do that.' })
+      return await done(403, {
+        message: 'You are not allowed to do that.',
+      })
     }
     const messages = await getMessages(database)
     return await done(200, messages)
   }
 
-
   if (req.method === 'POST') {
-    const message = await createMessage(req.body, settings.enableEmailingToAdmin, database)
+    const message = await createMessage(
+      req.body,
+      settings.enableEmailingToAdmin,
+      database
+    )
     return await done(200, message)
   }
 
