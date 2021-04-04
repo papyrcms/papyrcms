31a6b52492a14fa59c30319d1f76d230e3dc3734
drkgrntt <drkgrntt@gmail.com>
2021-04-04 07:22:36 -0500

    AdminDashboard scss module

--- src/components/AdminDashboard/index.tsx
+++ src/components/AdminDashboard/index.tsx
@@ -3,14 +3,15 @@ import AdminLinks from './AdminLinks'
 import AppSettingsForm from './AppSettingsForm'
 import UserList from './UserList'
 import MessageList from './MessageList'
+import styles from './AdminDashboard.module.scss'
 
 const AdminDashboard = () => (
-  <div className="admin-dashboard">
-    <div className="admin-dashboard__left">
+  <div className={styles.container}>
+    <div className={styles.left}>
       <AdminLinks />
     </div>
 
-    <div className="admin-dashboard__right">
+    <div className={styles.right}>
       <AppSettingsForm />
       <UserList />
       <MessageList />
