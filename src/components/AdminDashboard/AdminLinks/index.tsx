5e17981144dfe0c14a1c7774663d49b39a8b4a94
drkgrntt <drkgrntt@gmail.com>
2021-04-03 21:15:35 -0500

    Admin Links scss module

--- src/components/AdminDashboard/AdminLinks/index.tsx
+++ src/components/AdminDashboard/AdminLinks/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Link from 'next/link'
 import { settingsContext } from '@/context'
+import styles from './AdminLinks.module.scss'
 
 const AdminLinks: React.FC = () => {
   const { settings } = useContext(settingsContext)
@@ -10,15 +11,15 @@ const AdminLinks: React.FC = () => {
       return (
         <>
           <Link href="/store/new">
-            <a className="admin-links__link">Add Product</a>
+            <a className={styles.link}>Add Product</a>
           </Link>
 
           <Link href="/store">
-            <a className="admin-links__link">My Products</a>
+            <a className={styles.link}>My Products</a>
           </Link>
 
           <Link href="/store/orders">
-            <a className="admin-links__link">Orders</a>
+            <a className={styles.link}>Orders</a>
           </Link>
         </>
       )
@@ -30,11 +31,11 @@ const AdminLinks: React.FC = () => {
       return (
         <>
           <Link href="/blog/new">
-            <a className="admin-links__link">Add Blog</a>
+            <a className={styles.link}>Add Blog</a>
           </Link>
 
           <Link href="/blog/all">
-            <a className="admin-links__link">My Blogs</a>
+            <a className={styles.link}>My Blogs</a>
           </Link>
         </>
       )
@@ -46,11 +47,11 @@ const AdminLinks: React.FC = () => {
       return (
         <>
           <Link href="/events/new">
-            <a className="admin-links__link">Add Event</a>
+            <a className={styles.link}>Add Event</a>
           </Link>
 
           <Link href="/events/all">
-            <a className="admin-links__link">My Events</a>
+            <a className={styles.link}>My Events</a>
           </Link>
         </>
       )
@@ -60,19 +61,19 @@ const AdminLinks: React.FC = () => {
   return (
     <>
       <Link href="/admin/page-builder">
-        <a className="admin-links__link">Page Builder</a>
+        <a className={styles.link}>Page Builder</a>
       </Link>
 
       <Link href="/admin/pages">
-        <a className="admin-links__link">My Pages</a>
+        <a className={styles.link}>My Pages</a>
       </Link>
 
       <Link href="/posts/new">
-        <a className="admin-links__link">Add Content</a>
+        <a className={styles.link}>Add Content</a>
       </Link>
 
       <Link href="/posts">
-        <a className="admin-links__link">My Content</a>
+        <a className={styles.link}>My Content</a>
       </Link>
 
       {renderBlogMenuItems()}
