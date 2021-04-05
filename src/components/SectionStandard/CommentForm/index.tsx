8ac460a1fe5f675f1c7c66c548ce13edf36a10d1
drkgrntt <drkgrntt@gmail.com>
2021-04-04 20:15:07 -0500

    CommentForm scss module

---
+++ src/components/SectionStandard/CommentForm/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from './CommentForm.module.scss'
+
+type Props = {
+  content: string
+  onChange: Function
+  onSubmit: Function
+  detached: boolean
+  onDetachClick: Function
+}
+
+const CommentForm: React.FC<Props> = (props) => {
+  const {
+    content,
+    onChange,
+    onSubmit,
+    detached,
+    onDetachClick,
+  } = props
+
+  return (
+    <form
+      className={`${detached ? styles.detached : ''}`}
+      onSubmit={(event) => onSubmit(event)}
+    >
+      <label htmlFor="comment-text-editor" className={styles.label}>
+        Comment
+      </label>
+      {/* <RichTextEditor
+        id="comment-text-editor"
+        className=comment-form__text-editor"
+        content={content}
+        onChange={newContent => onChange(newContent)}
+      /> */}
+      <textarea
+        onChange={(newContent) => onChange(newContent.target.value)}
+        id="comment-text-editor"
+        className={styles.editor}
+        value={content}
+      ></textarea>
+
+      <div className={styles.bottom}>
+        <input className="button-primary" type="submit" />
+        <div>
+          <input
+            id="detach-checkbox"
+            className={styles.input}
+            type="checkbox"
+            onClick={() => onDetachClick()}
+          />
+          <label className={styles.detach} htmlFor="detach-checkbox">
+            {detached ? 'Attach' : 'Detach'}
+            <span className={styles.span}></span>
+          </label>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default CommentForm
--- src/components/SectionStandard/index.tsx
+++ src/components/SectionStandard/index.tsx
@@ -7,6 +7,7 @@ import Router from 'next/router'
 import renderHTML from 'react-render-html'
 import { userContext, postsContext, settingsContext } from '@/context'
 import { Media, PageHead } from '@/components'
+import CommentForm from './CommentForm'
 import { usePostFilter } from '@/hooks'
 import styles from './SectionStandard.module.scss'
 
@@ -477,69 +478,6 @@ const CommentComp: React.FC<CommentProps> = (props) => {
       {renderCommentForm()}
       {afterCommentForm()}
     </div>
-  )
-}
-
-type CommentFormProps = {
-  content: string
-  onChange: Function
-  onSubmit: Function
-  detached: boolean
-  onDetachClick: Function
-}
-
-const CommentForm: React.FC<CommentFormProps> = (props) => {
-  const {
-    content,
-    onChange,
-    onSubmit,
-    detached,
-    onDetachClick,
-  } = props
-
-  return (
-    <form
-      className={`comment-form ${detached ? 'detached' : ''}`}
-      onSubmit={(event) => onSubmit(event)}
-    >
-      <label
-        htmlFor="comment-text-editor"
-        className="comment-form__label"
-      >
-        Comment
-      </label>
-      {/* <RichTextEditor
-        id="comment-text-editor"
-        className=comment-form__text-editor"
-        content={content}
-        onChange={newContent => onChange(newContent)}
-      /> */}
-      <textarea
-        onChange={(newContent) => onChange(newContent.target.value)}
-        id="comment-text-editor"
-        className="comment-form__text-editor"
-        value={content}
-      ></textarea>
-
-      <div className="comment-form__bottom">
-        <input className="button button-primary" type="submit" />
-        <div className="comment-form__detach">
-          <input
-            id="detach-checkbox"
-            className="comment-form__checkbox--input"
-            type="checkbox"
-            onClick={() => onDetachClick()}
-          />
-          <label
-            className="comment-form__detach--label"
-            htmlFor="detach-checkbox"
-          >
-            {detached ? 'Attach' : 'Detach'}
-            <span className="comment-form__checkbox--span"></span>
-          </label>
-        </div>
-      </div>
-    </form>
   )
 }
 
