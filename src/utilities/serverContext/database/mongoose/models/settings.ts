22541a62aa2dab60c8755fd1d4a0bf9eb9963272
drkgrntt <drkgrntt@gmail.com>
2021-04-07 21:07:12 -0500

    Remove old models

--- src/utilities/serverContext/database/mongoose/models/settings.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const settingsSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  options: { type: Object, required: true }
-})
-
-export default mongoose.models.settings || mongoose.model('settings', settingsSchema)
